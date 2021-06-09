d3d6c32a5ae2a6886674160c4c579cf7d4d61ba2
tsantos- <tsantos-@student.42sp.org.br>
2021-06-09 22:57:01 +0000

    arquivos shell

---
+++ Shell/Shell01/ex08/add_chelou.sh
@@ -0,0 +1,1 @@
+FT_NBR1_TEMP="$(echo $FT_NBR1 | tr \''\\\"\?\!' '01234')" FT_NBR2_TEMP="$(echo $FT_NBR2 | tr 'mrdoc' '01234')" && echo "obase=13;ibase=5;$FT_NBR1_TEMP+$FT_NBR2_TEMP" | bc| tr '0123456789ABC' 'gtaio luSnemf'
